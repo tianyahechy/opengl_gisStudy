4e6450791e0421d5727cfcfd4749f248c7810b12
tianyahechy <tianyahechu2004@163.com>
2019-02-26 08:38:21 +0800

    包围盒和包围球

--- opengl_GIS/CELLFrameBigMap.h
+++ opengl_GIS/CELLFrameBigMap.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "CELLFrame.h"
 #include "CELLTimestamp.hpp"
+#include "CELLMath.hpp"
 namespace CELL
 {
 	class CELLFrameBigMap : public CELLFrame
 	{
+	public:
+		float3 _min;
+		double3 _max;
 	public:
 		CELLTimestamp _timeStamp;
 	public:
@@ -29,6 +33,40 @@ namespace CELL
 		virtual void onKeyDown(int key);
 		//键盘事件
 		virtual void onKeyUp(int key);
+		//计算包围盒数据
+		template< class T>
+		AxisAlignedBox<T> calcAabb(const tvec3<T>* pPos, uint nCount)
+		{
+			AxisAlignedBox<T> aabb;
+			aabb._minimum.x = FLT_MAX;
+			aabb._minimum.y = FLT_MAX;
+			aabb._minimum.z = FLT_MAX;
+			aabb._maximum.x = -FLT_MAX;
+			aabb._maximum.y = -FLT_MAX;
+			aabb._maximum.z = -FLT_MAX;
+			aabb._extent = AxisAlignedBox<T>::EXTENT_FINITE;
+
+			for (int i = 1; i < nCount; i++)
+			{
+				aabb._minimum.x = MIN(aabb._minimum.x, pPos[i].x);
+				aabb._minimum.y = MIN(aabb._minimum.y, pPos[i].y);
+				aabb._minimum.z = MIN(aabb._minimum.z, pPos[i].z);
+				aabb._maximum.x = MAX(aabb._maximum.x, pPos[i].x);
+				aabb._maximum.y = MAX(aabb._maximum.x, pPos[i].y);
+				aabb._maximum.z = MAX(aabb._maximum.x, pPos[i].z);
+			}
+			return aabb;
+		}
+		//计算包围球数据
+		template< class T>
+		tvec4<T> calcSphere(const tvec3<T>* pPos, uint nCount)
+		{
+			//先计算包围盒
+			AxisAlignedBox<T> aabb = calcAabb(pPos, nCount);
+			tvec3<T> center = aabb.getCenter();
+			T radius = aabb.getHalfSize();
+			return tvec4<T>(center.x, center.y, center.z, radius);
+		}
 
 	};
 }
