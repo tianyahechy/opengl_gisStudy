b17b45827fb9968770f90ab61901a0df5e1bef61
tianyahechy <tianyahechu2004@163.com>
2019-07-24 19:42:46 +0800

    这时候能调通完善摄像机之前的部分

---
+++ opengl_GIS/lifeiContext.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "lifeiPlatform.h"
+#include "CELLMath.hpp"
+#include "lifeiCamera.h"
+
+namespace CELL
+{
+	class CELLFrame;
+	class CELLResourceMgr;
+	class CELLOpenGL;
+	class CELLGIS3DPlatform;
+	class lifeiContext
+	{
+	public:
+		//全局的app
+		CELLGIS3DPlatform * _app;
+		//框架
+		CELLFrame *			_frame;
+		CELLOpenGL *		_device;
+		//资源管理者指针
+		CELLResourceMgr *	_resMgr;
+		//运行时目录
+		char				_pathEXE[CELL_PATH_LENGTH];
+		//资源目录
+		char				_pathRes[CELL_PATH_LENGTH];
+		//鼠标的当前位置
+		int					_mouseX;
+		//鼠标的当前位置
+		int					_mouseY;
+		//窗口的大小
+		int					_width;
+		int					_height;
+		//每一帧绘制所需要的时间
+		float				_timePerFrame;
+		//2d屏幕投影矩阵
+		matrix4r			_screenPrj;
+		//观察矩阵
+		lifeiCamera			_camera;
+		//mvp
+		matrix4r			_mvp;
+		matrix4r			_vp;
+		//视锥裁剪
+		Frustum				_frustum;
+		byte				_keyState[256];
+		lifeiContext()
+		{
+			memset(_keyState, 0, sizeof(_keyState));
+		}
+	};
+}
---
+++ opengl_GIS/lifeiInput.h
@@ -0,0 +1,31 @@
+
+#pragma once
+
+namespace CELL
+{
+	class lifeiInput
+	{
+	public:
+		//鼠标左键按下
+		virtual void onLButtonDown(int x, int y) = 0;
+		//鼠标左键提起
+		virtual void onLButtonUp(int x, int y) = 0;
+		//鼠标右键按下
+		virtual void onRButtonDown(int x, int y) = 0;
+		//鼠标右键提起
+		virtual void onRButtonUp(int x, int y) = 0;
+		//鼠标中键按下
+		virtual void onMButtonDown(int x, int y) = 0;
+		//鼠标中键提起
+		virtual void onMButtonUp(int x, int y) = 0;
+		//鼠标移动事件
+		virtual void onMouseMove(int x, int y) = 0;
+		//鼠标滚轮事件
+		virtual void onMouseWheel(int delta) = 0;
+		//键盘按键按下事件
+		virtual void onKeyDown(int key) = 0;
+		//键盘按键抬起事件
+		virtual void onKeyUp(int key) = 0;
+		
+	};
+}
