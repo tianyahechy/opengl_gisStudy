33ca3392444734838ac4861a3f14f0f04d3560cf
tianyahechy <tianyahechu2004@163.com>
2020-01-02 15:22:05 +0800

    lifeiMathUtil.h

--- opengl_GIS/CELLMath.hpp
+++ opengl_GIS/CELLMath.hpp
@@ -1,32 +1,8 @@
 #pragma once
-
-#include <cstdio>
-#include <cassert>
-#include <cmath>
-#include <vector>
-#include <map>
-#include <limits>
-#include <xutility>
+#include "lifeiMathUtil.h"
 
 namespace CELL
 {
-	typedef unsigned char			byte;
-    typedef long long               int64;
-    typedef unsigned long long      uint64;
-    typedef unsigned short          ushort;
-    typedef unsigned int            uint;
-    typedef unsigned long           ulong;
-
-    
-    #define PI                      3.14159265358979323846264338327
-    #define TWO_PI                  (PI * 2)
-    #define HALF_PI                 (3.14159265358979323846264338327 * 0.5)
-    
-    #define DEG2RAD(theta)          (0.01745329251994329 * theta)
-    #define RAD2DEG                 57.2957795130823208
-    #define LOG2                    0.69314718055994529
-    #define WGS_84_RADIUS_EQUATOR_  6378137.0
-    #define WGS_84_RADIUS_POLAR_    6356752.3142
     #define MIN(a,b)                ((a) < (b) ? (a) : (b))
     #define MAX(a,b)                ((a) > (b) ? (a) : (b))
     #define MAX_USHORT              65536
